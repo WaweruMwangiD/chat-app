96a3757bc9fbdb1841cead5914c9f921d6d5a711
WaweruMwangiD <wawerumwangi21@gmail.com>
2022-06-26 20:44:10 +0300

    modified the chat component

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,4 +1,4 @@
-import { AttachFile, MoreVert, SearchOutlined } from '@mui/icons-material'
+import { AttachFile, InsertEmoticonOutlined, MicOutlined, MoreVert, SearchOutlined } from '@mui/icons-material'
 import { Avatar, IconButton } from '@mui/material'
 import React from 'react'
 import './Chat.css'
@@ -25,12 +25,47 @@ function Chat() {
         </div>
       </div>
       <div className='chat_body'>
+
+
+      <p className='chat_message chat_receiver'>
+          <span className='chat_name'>Helloooo Dave</span>
+          <span className='chat_timestamp'>
+              {new Date().toUTCString()}
+          </span>
+        </p>
         <p className='chat_message'>
           <span className='chat_name'>Helloooo Dave</span>
           <span className='chat_timestamp'>
-            
+              {new Date().toUTCString()}
           </span>
         </p>
+        <p className='chat_message'>
+          <span className='chat_name'>Helloooo Dave</span>
+          <span className='chat_timestamp'>
+              {new Date().toUTCString()}
+          </span>
+        </p>
+        <p className='chat_message chat_receiver'>
+          <span className='chat_name'>Helloooo Dave</span>
+          <span className='chat_timestamp'>
+              {new Date().toUTCString()}
+          </span>
+        </p>
+      </div>
+      <div className='chat_footer'>
+          <IconButton>
+            <InsertEmoticonOutlined/>
+          </IconButton>
+          <form>
+            <input type='text' placeholder='message' />
+            <button type='submit'>Send message</button>
+          
+          </form>
+          
+          <IconButton>
+            <MicOutlined />
+          </IconButton>
+
       </div>
     
     </div>
